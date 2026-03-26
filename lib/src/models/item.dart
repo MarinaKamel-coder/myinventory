72d7453598a527b80a7049a35b953c1d116ddb67
Sonia Corbin <soniacorbin4@gmail.com>
2026-03-26 13:41:52 -0400

    feat: établir une relation stable box-item avec boxId bidirectionnel

--- lib/src/models/item.dart
+++ lib/src/models/item.dart
@@ -1,28 +1,46 @@
 class Item {
   const Item({
     required this.id,
+    required this.boxId,
     required this.name,
     this.description,
     this.photo,
   });
 
   final String id;
+  final String boxId;
   final String name;
   final String? description;
   final String? photo;
 
+  Item copyWith({
+    String? id,
+    String? boxId,
+    String? name,
+    String? description,
+    String? photo,
+  }) =>
+      Item(
+        id: id ?? this.id,
+        boxId: boxId ?? this.boxId,
+        name: name ?? this.name,
+        description: description ?? this.description,
+        photo: photo ?? this.photo,
+      );
+
   @override
-  String toString() => 'Item(id: $id, name: $name)';
+  String toString() => 'Item(id: $id, boxId: $boxId, name: $name)';
 
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
       other is Item &&
           other.id == id &&
+          other.boxId == boxId &&
           other.name == name &&
           other.description == description &&
           other.photo == photo;
 
   @override
-  int get hashCode => Object.hash(id, name, description, photo);
+  int get hashCode => Object.hash(id, boxId, name, description, photo);
 }
